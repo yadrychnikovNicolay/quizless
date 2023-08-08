c6314cd7353f5bbdd1f81da784cef2308089face
yadrychnikovNicolay <yadrychnikov.n@gmail.com>
2023-08-08 13:33:36 +0200

    changed input system

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-const cards = document.querySelectorAll('.SetPageTerm-wordText');
-const backCards = document.querySelectorAll('.SetPageTerm-definitionText')
 const flash_display = document.getElementById('flash_display')
 const flash_controls_back = document.getElementById('flash_controls_back')
 const flash_controls_flip = document.getElementById('flash_controls_flip')
 const flash_controls_next = document.getElementById('flash_controls_next')
 const flash_count = document.getElementById('flash_count')
 
-let data = []
-
-for(let i = 0; i < cards.length; i++) {
-    data.push({
-        "front": cards[i].innerText,
-        "back": backCards[i].innerText
-    })
+let fetchData = async() => {
+    const response = await fetch('./azure_sc_900.json');
+    const data = await response.json();
+    console.log(data)
+    return data
 }
 
-const flipCard = () => {
+const initFlipper = data => {
+    flash_display.innerText = data[0].front
+    flash_controls_flip.addEventListener('click', () => {flipCard(data)})
+    flash_controls_back.addEventListener('click', () => {handleControls("prev", data)})
+    flash_controls_next.addEventListener('click', () => {handleControls("next", data)})
+    flash_count.innerText = `1/${data.length}`
+}
+
+let data = fetchData().then(data => initFlipper(data))
+
+const flipCard = data => {
     console.log('flipCardCalled: ', flash_display.dataset)
     if(flash_display.dataset.cardside == "back") {
         flash_display.innerText = data[flash_display.dataset.cardid].front
@@ -26,7 +32,7 @@ const flipCard = () => {
     }
 }
 
-const handleControls = direction => {
+const handleControls = (direction, data) => {
     const cardId = parseInt(flash_display.dataset.cardid)
     const cardSide = flash_display.dataset.cardside
     const newIndex = (((direction == "next" ? cardId+1 : cardId-1) % data.length) + data.length) % data.length
@@ -34,14 +40,3 @@ const handleControls = direction => {
     flash_display.dataset.cardid = newIndex
     flash_count.innerText = `${newIndex+1}/${data.length}`
 }
-
-const initFlipper = data => {
-    flash_display.innerText = data[0].front
-    flash_controls_flip.addEventListener('click', flipCard)
-    flash_controls_back.addEventListener('click', () => {handleControls("prev")})
-    flash_controls_next.addEventListener('click', () => {handleControls("next")})
-    flash_count.innerText = `1/${data.length}`
-
-}
-
-initFlipper(data)
