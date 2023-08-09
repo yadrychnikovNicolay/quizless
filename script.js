cc03a1a296adf9065111d88ec3014f18941d14b4
YADRYCHNIKOV Nicolay <33456276+yadrychnikovNicolay@users.noreply.github.com>
2023-08-09 09:47:44 +0200

    remove logs and user select

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const flash_count = document.getElementById('flash_count')
 let fetchData = async() => {
     const response = await fetch('./azure_sc_900.json');
     const data = await response.json();
-    console.log(data)
     return data
 }
 
@@ -23,7 +22,6 @@ const initFlipper = data => {
 let data = fetchData().then(data => initFlipper(data))
 
 const flipCard = data => {
-    console.log('flipCardCalled: ', flash_display.dataset)
     if(flash_display.dataset.cardside == "back") {
         flash_display.innerText = data[flash_display.dataset.cardid].front
         flash_display.dataset.cardside = "front"
